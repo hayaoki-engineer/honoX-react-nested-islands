2e41c8f4c46ab29af4cd1c80af8f490441b58b6c
Taisei <taisei.960420@gmail.com>
2024-09-21 09:41:13 +0900

    Reactレンダラーを適用

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,38 +1,19 @@
-import { Style } from 'hono/css'
-import { jsxRenderer } from 'hono/jsx-renderer'
-import { Script } from 'honox/server'
+import { reactRenderer } from "@hono/react-renderer";
 
-export default jsxRenderer(({ children, title }) => {
+export default reactRenderer(({ children, title }) => {
   return (
     <html lang="en">
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
         {import.meta.env.PROD ? (
-          <script type="module" src='/sta'
+          <script type="module" src="/static/client.js"></script>
+        ) : (
+          <script type="module" src="/app/client.ts"></script>
         )}
-        <Script src="/app/client.ts" async />
-        <Style />
+        {title && <title>{title}</title>}
       </head>
       <body>{children}</body>
     </html>
-  )
-})
-
-/*
-
-- hono/jsx-renderer or hono/react-renderer
-
-# hono/jsx-render
-
-何をしている？
-
-import { Script } from 'honox/serverと一緒に
-
-# hono/react-renderer
-
-何をしている？
-
-*/
+  );
+});
